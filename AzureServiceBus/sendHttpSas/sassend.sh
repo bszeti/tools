1e9c1007d392a66e4066cc9024ff08279711275e
Balazs Szeti <bszeti@redhat.com>
2018-02-14 12:06:34 -0500

    How to to read message from topic with curl

--- AzureServiceBus/sendHttpSas/sassend.sh
+++ AzureServiceBus/sendHttpSas/sassend.sh
@@ -26,3 +26,17 @@ SAS="sr=$SBURLENCODED&sig=$(urlencode $HMAC)&se=$TIMESTAMP&skn=$POLICY"
 
 echo $SAS
 curl -vvv -X POST -H "Content-Type: application/xml" -H "Authorization: SharedAccessSignature $SAS" -d "<HELLO/>" $HTTPURL
+
+#If policy has "Manage" for this topic, can get info about topic
+# curl -s -H "Authorization: SharedAccessSignature $SAS" $HTTPURL/
+# curl -s -H "Authorization: SharedAccessSignature $SAS" $HTTPURL/subscriptions
+# curl -s -H "Authorization: SharedAccessSignature $SAS" $HTTPURL/subscriptions/mysubscription
+
+#If policy has "Listen", can read messages
+#Take and remove message:
+# curl -s -H "Authorization: SharedAccessSignature $SAS" -X DELETE $HTTPURL/subscriptions/mysubscription/messages/head
+#Lock, process and then remove message (by Location response header):
+# curl -vvv -s -H "Authorization: SharedAccessSignature $SAS" -X POST -d "" $HTTPURL/subscriptions/mysubscription/messages/head
+# curl -s -H "Authorization: SharedAccessSignature $SAS" -X DELETE $HTTPURL/subscriptions/mysubscription/messages/2/7da9cfd5-40d5-4bb1-8d64-ec5a52e1c547
+#Unlock (put back)
+# curl -s -H "Authorization: SharedAccessSignature $SAS" -X PUT $HTTPURL/subscriptions/mysubscription/messages/2/7da9cfd5-40d5-4bb1-8d64-ec5a52e1c547
