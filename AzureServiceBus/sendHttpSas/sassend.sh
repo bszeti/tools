e1adaa22ed37600d3a78809dafa8b44dd324dd24
Balazs Szeti <bszeti@redhat.com>
2018-02-14 10:52:48 -0500

    Add bash script to send Azure ServiceBus topic with generated SharedAccessSignature authentication

---
+++ AzureServiceBus/sendHttpSas/sassend.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -x
+# Send to an Azure Topic using SharedAccessSignature
+# This code genreates the signature from key
+# See (wrong hmac code example) https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-sas
+# See (correct hmac code example) https://docs.microsoft.com/en-us/rest/api/eventhub/generate-sas-token
+
+#urlencode
+function urlencode() {
+	echo -n "$1" | perl -MURI::Escape -ne 'print uri_escape($_)'
+}
+
+#Inputs
+RESOURCE='bszeti.servicebus.windows.net/first'
+KEY='+E948RpHAtIasfjsZdEM2utwmv9Flk7hkOFEhKIxk5s='
+POLICY=mypolicy
+TIMESTAMP=1548251906
+
+HTTPURL="https://$RESOURCE/messages"
+#url encoded resource is required for HMAC too
+SBURLENCODED=$(urlencode "sb://$RESOURCE")
+
+
+#The b64 encoded key is used, (no need to b64->byte->hex convert)
+HMAC=$(echo -n -e "$SBURLENCODED\n$TIMESTAMP" | openssl sha256 -mac HMAC -macopt key:$KEY -binary | base64)
+SAS="sr=$SBURLENCODED&sig=$(urlencode $HMAC)&se=$TIMESTAMP&skn=$POLICY"
+
+echo $SAS
+curl -vvv -X POST -H "Content-Type: application/xml" -H "Authorization: SharedAccessSignature $SAS" -d "<HELLO/>" $HTTPURL
