8356e8e444b2c6f3d75ccb1535976acacff50e50
Balazs Szeti <bszeti@redhat.com>
2018-03-03 17:43:21 -0500

    use one file for oc resources

--- OpenShift/jenkins-build/run.sh
+++ OpenShift/jenkins-build/run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-oc create -f ./git-ssh.template
-oc create -f ./git-secret.template
-oc create -f ./git-config.template
-oc create -f ./maven-secret.template
+oc create -f ./oc-resources.yaml
 oc create configmap maven-settings --from-file=settings.xml=settings.xml --from-file=myTruststore.jks=myTruststore.jks
